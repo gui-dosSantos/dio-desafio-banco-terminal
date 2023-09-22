f5ecf5be05dca1cf5242de92a59c135a7ddd22b4
gui-dosSantos <guilherme.franca0@gmail.com>
2023-09-22 11:05:55 -0300

    feat: scanner fechado ao final da função main, mudança da lógica de input e output para uma função própria e adição de tratamento de exceção

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Locale;
 import java.text.DecimalFormatSymbols;
@@ -12,23 +13,43 @@ public class ContaTerminal {
         //Uso da função useDelimiter foi feito para que a leitura do nome do cliente aceitasse o uso de espaços
         //Permitindo assim a inserção de nomes compostos ou nome + sobrenome
         Scanner scanner = new Scanner(System.in).useDelimiter("\n").useLocale(Locale.US);
+        imprimirInformacaoConta(scanner);
+        scanner.close();
+    }
 
-        System.out.println("Por favor, digite o número da sua agência:");
-        String agencia = scanner.next();
+    private static void imprimirInformacaoConta(Scanner scanner){
+        final int TENTATIVAMAXIMA = 3;
+        int tentativa = 0;
 
-        System.out.println("Por favor, digite o número da sua conta:");
-        int conta = scanner.nextInt();
+        while(tentativa < TENTATIVAMAXIMA){
+            try {
+                System.out.println("Por favor, digite o número da sua agência:");
+                String agencia = scanner.next();
 
-        System.out.println("Por favor, digite o seu nome:");
-        String nomeCliente = scanner.next();
+                System.out.println("Por favor, digite o número da sua conta:");
+                int conta = scanner.nextInt();
 
-        System.out.println("Por favor, digite o seu saldo:");
-        double saldo = scanner.nextDouble();
+                System.out.println("Por favor, digite o seu nome:");
+                String nomeCliente = scanner.next();
 
-        //Formatação do saldo para exibição de acordo com a exibição padrão de moedas, com 2 casas decimais
-        String saldoFormatado = padraoFormatacaoDecimal.format(saldo);
+                System.out.println("Por favor, digite o seu saldo:");
+                double saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldoFormatado + " já está disponível para saque");
+                //Formatação do saldo para exibição de acordo com a exibição padrão de moedas, com 2 casas decimais
+                String saldoFormatado = padraoFormatacaoDecimal.format(saldo);
+
+                System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo $" + saldoFormatado + " já está disponível para saque");
+                break;
+            } catch (InputMismatchException e) {
+                tentativa++;
+                System.out.println("Os valores inseridos para a conta e para o saldo devem estar em formato numérico. Tentativas restantes: " + (TENTATIVAMAXIMA - tentativa));
+                scanner.nextLine();
+            }
+        }
+        if(tentativa == TENTATIVAMAXIMA){
+            System.out.println("Número máximo de tentativas excedido. Entre em contato com o seu gerente.");
+        }
+
     }
 
 }
